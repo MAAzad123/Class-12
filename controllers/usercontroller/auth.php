8f011fa1a750ebe785a86bad09cee9c9008efeb2
MAAzad123 <143494752+MAAzad123@users.noreply.github.com>
2023-10-10 23:09:08 +0600

    Add files via upload

---
+++ controllers/usercontroller/auth.php
@@ -0,0 +1,38 @@
+<?php 
+    session_start();
+    include_once './../../../env.php'; 
+    $email = trim($_REQUEST['email']);
+    $password = $_REQUEST['password'];
+    $hashPassword = password_hash ($password, PASSWORD_BCRYPT);
+    $errors = [];
+
+
+
+
+//Email Validation
+if (empty($email)){
+    $errors ['emailError'] = 'Email is required';
+}elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $errors ['emailError'] = 'Invalid Email Address';
+}
+
+//Password Validation
+if (empty($password)){
+    $errors ['passwordError'] = 'Password is required';
+}elseif (strlen($password)<8){
+    $errors ['passwordError'] = 'Password can not be less than 8 charactr';
+}
+
+
+
+if (count($errors)>0){
+    $_SESSION = $errors;
+    header ('Location: ./../../backend/login.php');
+}else {
+    $query = "SELECT email, password FROM users WHERE id = 1";
+    $result = mysqli_query($conn, $query);
+    $data = mysqli_fetch_assoc($result);
+
+}
+
+?>
